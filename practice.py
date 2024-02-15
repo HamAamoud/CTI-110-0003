1a66c52da66c9fe4e99a2d8ee51b85b06837da39
HamAamoud <160150191+HamAamoud@users.noreply.github.com>
2024-02-15 16:05:08 +0000

    Create practice

---
+++ practice.py
@@ -0,0 +1,9 @@
+#Hard coded value - value is the same everytime code is run
+student1 = "Bob"
+
+#User input - value is assigned the users input
+student2 = input("Enter a students name: ")
+
+print("The first students name is",student1)
+
+print("The second students name is",student2)
