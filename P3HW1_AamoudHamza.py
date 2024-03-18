191bb8667a6bd94552fe993c82c9d719645c5ba7
HamAamoud <160150191+HamAamoud@users.noreply.github.com>
2024-03-18 00:02:22 +0000

    P3HW1 complete

--- P3HW1_AamoudHamza.py
+++ P3HW1_AamoudHamza.py
@@ -1,40 +1,50 @@
-# I was supposed to put a comment here
-# My Last Name
-
+#Hamza Aamoud
+#3/17/2024
+#P3HW1
+#Fix errors in code
 
 # This program takes a number grade , determines average and displays letter grade for average.
 
 # Enter grades for six modules
 
-mod_1 = input('Enter grade for Module 1: ')
- mod_2 = input('Enter grade for Module 2: ')
-mod_1 = input('Enter grade for Module 3: )
-mod 4 = input('Enter grade for Module 1: ')
-mod_1 = input('Enter grade for Module 5: ')
- mod_1 = input('Enter grade for Module 6: ")
+mod_1 = float(input('Enter grade for Module 1: '))
+mod_2 = float(input('Enter grade for Module 2: '))
+mod_3 = float(input('Enter grade for Module 3: '))
+mod_4 = float(input('Enter grade for Module 4: '))
+mod_5 = float(input('Enter grade for Module 5: '))
+mod_6 = float(input('Enter grade for Module 6: '))
 
 # add grades entered to a list
 
-grades = [mod_1 mod2, mod_3, mod_4, Mod_5]
+grades = [mod_1,mod_2,mod_3,mod_4,mod_5,mod_6]
 # TO DO: determine lowest, highest , sum and average for grades
 
-low = min(Grades)
-high = high(grades)
+low = min(grades)
+high = max(grades)
 sum = sum(grades)
-avg =
+avg = sum / len(grades)
 
 # determine letter grade for average
 
-
+print('------Results------')
+print("Lowest Grade:     ",low)
+print("Higfhest Grade:   ",high)
+print("Sum of Grades:    ",sum)
+print(f'Average:           {avg:.2f}')
+print('-------------------')
 if avg >= 90:
-print('Your grade is: A')
+    print('Your grade is: A')
 else:
-if average > 80:
- print('Your grade is: B')
-else:
-
-else:
-print('Your grade is: F') # TO DO: finish this
+    if avg >= 80:
+        print('Your grade is: B')
+    else:
+        if avg >= 70:
+            print('Your grade is: C')
+        else:
+            if avg >= 60:
+                print('Your grade is: D')
+            else:
+                print('Your grade is: F') # TO DO: finish this
 
 
 
