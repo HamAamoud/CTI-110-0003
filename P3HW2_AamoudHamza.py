d0cb12bd2bd7de1d15ffcae82ca0ff21cf2458d2
HamAamoud <160150191+HamAamoud@users.noreply.github.com>
2024-03-24 04:46:56 +0000

    Complete

--- P3HW2_AamoudHamza.py
+++ P3HW2_AamoudHamza.py
@@ -4,7 +4,22 @@
 #Calculating pay using if statements for overtime
 
 '''
+Enter name
+Enter hours
+Enter payRate
 
+If hours <= 40 then
+    normPay = hours * rate
+    grossPay = normPay
+else
+    normPay = rate * 40
+    overtimeHours = hour - 40
+    overtimePay = overtimeHours * (rate * 1.5)
+    grossPay = normPay + overtimePay
+
+Display Name
+Display "Hours Worked    Pay Rate   Overtime Hours   Overtime Pay   Regular Pay   Gross Pay"
+Display  hours           payRate    overtimeHours    overtimePay    normPay       grossPay   
 '''
 
 #Inputs/variables
@@ -25,6 +40,9 @@ elif worked > 40 :
     grossPay = normPay + otPay
 
 #Output
-print("------------------------------------------------------------------------------------------------------------------")
+print("-----------------------------------------------------------------------------------------------")
 print(f'Employee Name : {eName}')
-print("------------------------------------------------------------------------------------------------------------------")
+print("")
+print( "Hours Worked     Pay Rate     Overtime Hours     Overtime Pay     Regular Pay     Gross Pay")
+print("-----------------------------------------------------------------------------------------------")
+print(f'{worked:<17}${rate:<12.2f}{otHours:<19}${otPay:<16.2f}${normPay:<15.2f}${grossPay:.2f}')
