62fb657289720e6b175260cb7c66c4433e5c9f7d
HamAamoud <160150191+HamAamoud@users.noreply.github.com>
2024-03-28 14:37:56 +0000

    done

--- P4LAB2_AamoudHamza.py
+++ P4LAB2_AamoudHamza.py
@@ -6,14 +6,11 @@
 num1 = int(input("Enter a starting number: "))
 num2 = int(input("Enter a target number: "))
 
-l = 1
-while l == 1:
-    if num1 < num2:
-        l = 0
-        while num1 <= num2:
-            print(num1)
-            num1 += 5
-    else:
-        print("The first number must be smaller!!!")
-        num1 = int(input("Enter a starting number: "))
-        num2 = int(input("Enter a target number: "))
+
+while num1 > num2:
+    print("The first number must be smaller!!!")
+    num1 = int(input("Enter a starting number: "))
+    num2 = int(input("Enter a target number: "))
+
+for number in range(num1, num2+1,5):
+    print(number)
